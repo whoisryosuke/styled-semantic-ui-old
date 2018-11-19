5bacc6bd0dab4f976100479643177ae5ea59258e
Ryosuke <ryosuke.san.hana@gmail.com>
2018-11-18 22:07:24 -0800

    :lipstick: Swapped from JSS to Styled Components

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,46 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import injectSheet from "react-jss";
-
-const styles = theme => ({
-  myButton: {
-    color: theme.text.color,
-    margin: {
-      // jss-expand gives more readable syntax
-      top: 5, // jss-default-unit makes this 5px
-      right: 0,
-      bottom: 0,
-      left: "1rem"
-    },
-    "& span": {
-      // jss-nested applies this to a child span
-      fontWeight: "bold" // jss-camel-case turns this into 'font-weight'
-    }
-  },
-  myLabel: {
-    fontStyle: "italic"
-  }
-});
 
 /**
  * Button
  *
  */
-const Button = ({ classes, children }) => (
-  <button type="button" className={classes.myButton}>
-    <span className={classes.myLabel}>{children}</span>
+const Button = ({ children }) => (
+  <button type="button">
+    <span>{children}</span>
   </button>
 );
 
 Button.propTypes = {
-  /**
-   * CSS Class names provided by JSS
-   */
-  classes: PropTypes.shape({
-    myButton: PropTypes.string.isRequired,
-    myLabel: PropTypes.string.isRequired
-  }).isRequired,
-
   /**
    * Child components (array or single element)
    */
@@ -50,4 +21,4 @@ Button.propTypes = {
   ]).isRequired
 };
 
-export default injectSheet(styles)(Button);
+export default Button;
